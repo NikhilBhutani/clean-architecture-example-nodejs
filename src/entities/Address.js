635a8c1f97ff7f28c872a5e5d60f22042b0f574b
Brace Larsen <brace.larsen@gmail.com>
2017-11-21 19:33:25 -0800

    Docs make it easier for the IDE to auto-complete and warn of type mismatching

--- src/entities/Address.js
+++ src/entities/Address.js
@@ -1,37 +1,52 @@
-const { attributes } = require("structure"),
+/**
+ * @module ./src/entities/Address
+ */
 
-      Address        = attributes({
-        streetNumber: {
-          type:     Number,
-          integer:  true,
-          greater:  0,
-          required: true,
-        },
-        streetName:   {
-          type:     String,
-          required: true,
-        },
-        city:         {
-          type:     String,
-          required: true,
-        },
-        state:        {
-          type:     String,
-          required: true,
-        },
-        zipCode:      {
-          type:     Number,
-          integer:  true,
-          min:      0,
-          max:      99999,
-          required: true,
-        },
-      })(
-        class Address {
-          get singleLineFormat () {
-            return `${this.streetNumber} ${this.streetName}, ${this.city}, ${this.state} ${this.zipCode}`;
-          }
-        },
-      );
+const
+  { attributes } = require("structure"),
+
+  Address        = attributes({
+    streetNumber: {
+      type:     Number,
+      integer:  true,
+      greater:  0,
+      required: true,
+    },
+    streetName:   {
+      type:     String,
+      required: true,
+    },
+    city:         {
+      type:     String,
+      required: true,
+    },
+    state:        {
+      type:     String,
+      required: true,
+    },
+    zipCode:      {
+      type:     Number,
+      integer:  true,
+      min:      0,
+      max:      99999,
+      required: true,
+    },
+  })(
+    /**
+     * @member {Number} streetNumber
+     * @member {Number} streetName
+     * @member {Number} city
+     * @member {Number} state
+     * @member {Number} zipCode
+     */
+    class Address {
+      /**
+       * @returns {String} - A single line formatted address.
+       */
+      get singleLineFormat () {
+        return `${this.streetNumber} ${this.streetName}, ${this.city}, ${this.state} ${this.zipCode}`;
+      }
+    },
+  );
 
 module.exports = Address;
