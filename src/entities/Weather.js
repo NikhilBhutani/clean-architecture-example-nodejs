ec4b6aed521fb1767f116f85bc87dcd4946d64b7
Brace Larsen <brace.larsen@gmail.com>
2017-11-22 13:58:08 -0800

    The weather high temperature has some validity rules

--- src/entities/Weather.js
+++ src/entities/Weather.js
@@ -7,10 +7,15 @@ const
   { attributes } = require("structure"),
 
   Weather        = attributes({
-    dateTime: {
+    dateTime:        {
       type:     Date,
       min:      1,
       required: true,
+    },
+    daytimeHighTemp: {
+      type:      Number,
+      precision: 2,
+      required:  true,
     },
   })(
     class Weather {
--- test/entities/Weather.spec.js
+++ test/entities/Weather.spec.js
@@ -13,7 +13,8 @@ suite(`Entity :: Weather`, () => {
 
   beforeEach(async () => {
     fakeValidWeatherAttrs = {
-      dateTime: 42,
+      dateTime:        42,
+      daytimeHighTemp: 44,
     };
   });
 
@@ -121,6 +122,172 @@ suite(`Entity :: Weather`, () => {
           and.to.be.instanceof(fakeWeather);
         expect(fakeWeatherEntity[fieldNameToTest]).
           and.to.be.undefined();
+        expect(fakeValid).
+          and.to.be.a.boolean().
+          and.to.be.false();
+        expect(fakeErrors).
+          and.not.to.be.empty().
+          and.to.be.an.array();
+      });
+    });
+
+    suite(`daytimeHighTemp`, () => {
+      const fieldNameToTest = "daytimeHighTemp";
+
+      test(`should be "valid" when the type of value is a positive "integer"`, async () => {
+        // Conditions
+        fakeValidWeatherAttrs[fieldNameToTest] = 44;
+        const fakeWeatherEntity                = new fakeWeather(fakeValidWeatherAttrs);
+
+        // Interrogate a response
+        const { valid: fakeValid, errors: fakeErrors } = fakeWeatherEntity.validate();
+
+        // Assertions
+        expect(fakeWeatherEntity).
+          and.not.to.be.undefined().
+          and.to.be.instanceof(fakeWeather);
+        expect(fakeWeatherEntity[fieldNameToTest]).
+          and.not.to.be.undefined().
+          and.to.be.a.number().
+          and.to.equal(44);
+        expect(fakeValid).
+          and.to.be.a.boolean().
+          and.to.be.true();
+        expect(fakeErrors).
+          and.to.be.undefined();
+      });
+
+      test(`should be "valid" when the type of value is a negative "integer"`, async () => {
+        // Conditions
+        fakeValidWeatherAttrs[fieldNameToTest] = -44;
+        const fakeWeatherEntity                = new fakeWeather(fakeValidWeatherAttrs);
+
+        // Interrogate a response
+        const { valid: fakeValid, errors: fakeErrors } = fakeWeatherEntity.validate();
+
+        // Assertions
+        expect(fakeWeatherEntity).
+          and.not.to.be.undefined().
+          and.to.be.instanceof(fakeWeather);
+        expect(fakeWeatherEntity[fieldNameToTest]).
+          and.not.to.be.undefined().
+          and.to.be.a.number().
+          and.to.equal(-44);
+        expect(fakeValid).
+          and.to.be.a.boolean().
+          and.to.be.true();
+        expect(fakeErrors).
+          and.to.be.undefined();
+      });
+
+      test(`should be "valid" when the type of value is a positive "decimal"`, async () => {
+        // Conditions
+        fakeValidWeatherAttrs[fieldNameToTest] = 44.42;
+        const fakeWeatherEntity                = new fakeWeather(fakeValidWeatherAttrs);
+
+        // Interrogate a response
+        const { valid: fakeValid, errors: fakeErrors } = fakeWeatherEntity.validate();
+
+        // Assertions
+        expect(fakeWeatherEntity).
+          and.not.to.be.undefined().
+          and.to.be.instanceof(fakeWeather);
+        expect(fakeWeatherEntity[fieldNameToTest]).
+          and.not.to.be.undefined().
+          and.to.be.a.number().
+          and.to.equal(44.42);
+        expect(fakeValid).
+          and.to.be.a.boolean().
+          and.to.be.true();
+        expect(fakeErrors).
+          and.to.be.undefined();
+      });
+
+      test(`should be "valid" when the type of value has a precision beyond 2 places`, async () => {
+        // Conditions
+        fakeValidWeatherAttrs[fieldNameToTest] = 44.423;
+        const fakeWeatherEntity                = new fakeWeather(fakeValidWeatherAttrs);
+
+        // Interrogate a response
+        const { valid: fakeValid, errors: fakeErrors } = fakeWeatherEntity.validate();
+
+        // Assertions
+        expect(fakeWeatherEntity).
+          and.not.to.be.undefined().
+          and.to.be.instanceof(fakeWeather);
+        expect(fakeWeatherEntity[fieldNameToTest]).
+          and.not.to.be.undefined().
+          and.to.be.a.number().
+          and.to.equal(44.423);
+        expect(fakeValid).
+          and.to.be.a.boolean().
+          and.to.be.false();
+        expect(fakeErrors).
+          and.not.to.be.empty().
+          and.to.be.an.array();
+      });
+
+      test(`should be "invalid" with a default value of 0 when the value is null`, async () => {
+        // Conditions
+        fakeValidWeatherAttrs[fieldNameToTest] = null;
+        const fakeWeatherEntity                = new fakeWeather(fakeValidWeatherAttrs);
+
+        // Interrogate a response
+        const { valid: fakeValid, errors: fakeErrors } = fakeWeatherEntity.validate();
+
+        // Assertions
+        expect(fakeWeatherEntity).
+          and.not.to.be.undefined().
+          and.to.be.instanceof(fakeWeather);
+        expect(fakeWeatherEntity[fieldNameToTest]).
+          and.not.to.be.undefined().
+          and.to.be.a.number().
+          and.to.equal(0);
+        expect(fakeValid).
+          and.to.be.a.boolean().
+          and.to.be.true();
+        expect(fakeErrors).
+          and.to.be.undefined();
+      });
+
+      test(`should be "valid" with a default value of 0 when the value is null`, async () => {
+        // Conditions
+        fakeValidWeatherAttrs[fieldNameToTest] = null;
+        const fakeWeatherEntity                = new fakeWeather(fakeValidWeatherAttrs);
+
+        // Interrogate a response
+        const { valid: fakeValid, errors: fakeErrors } = fakeWeatherEntity.validate();
+
+        // Assertions
+        expect(fakeWeatherEntity).
+          and.not.to.be.undefined().
+          and.to.be.instanceof(fakeWeather);
+        expect(fakeWeatherEntity[fieldNameToTest]).
+          and.not.to.be.undefined().
+          and.to.be.a.number().
+          and.to.equal(0);
+        expect(fakeValid).
+          and.to.be.a.boolean().
+          and.to.be.true();
+        expect(fakeErrors).
+          and.to.be.undefined();
+      });
+
+      test(`should be "invalid" when undefined`, async () => {
+        // Conditions
+        delete fakeValidWeatherAttrs[fieldNameToTest];
+        const fakeWeatherEntity = new fakeWeather(fakeValidWeatherAttrs);
+
+        // Interrogate a response
+        const { valid: fakeValid, errors: fakeErrors } = fakeWeatherEntity.validate();
+
+        // Assertions
+        expect(fakeWeatherEntity).
+          and.not.to.be.undefined().
+          and.to.be.instanceof(fakeWeather);
+        expect(fakeWeatherEntity[fieldNameToTest]).
+          and.to.be.undefined().
+          and.not.to.be.a.number();
         expect(fakeValid).
           and.to.be.a.boolean().
           and.to.be.false();
