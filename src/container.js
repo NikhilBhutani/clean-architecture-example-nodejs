5be9a84b46ac6d8a2617652bf4366f4e3a8b6eef
Brace Larsen <brace.larsen@gmail.com>
2017-11-30 13:53:41 -0800

    Resolving dependency chains are easier to compose with a DI container

---
+++ src/container.js
@@ -0,0 +1,70 @@
+/**
+ * @module ./src/container
+ */
+
+const
+  { scopePerRequest }                 = require("awilix-express"),
+  {
+    createContainer,
+    Lifetime,
+    asClass,
+    asFunction,
+    asValue,
+  }                                   = require("awilix"),
+  container                           = createContainer(),
+
+  { AddressWeatherHistoryInteractor } = require("./usecases/AddressWeatherHistory"),
+
+  DailyWeatherPresenter               = require("./http/presenters/DailyWeather"),
+
+  { ConfigGateway }                   = require("./gateways/Config"),
+  { GeocoderGateway }                 = require("./gateways/Geocoder"),
+  { TimezoneGateway }                 = require("./gateways/Timezone"),
+  { WeatherGateway }                  = require("./gateways/Weather"),
+
+  Server                              = require("./http/Server"),
+  router                              = require("./http/router"),
+
+  Environment                         = require("./externals/Environment"),
+  { WeatherApi }                      = require("./externals/DarkSky"),
+  {
+    GeocoderApi,
+    TimezoneApi,
+  }                                   = require("./externals/Google");
+
+// Compose the dependency cradle
+container.
+  register({
+    // System
+    configGateway: asClass(ConfigGateway, { lifetime: Lifetime.SINGLETON }),
+    environment:   asClass(Environment, { lifetime: Lifetime.SINGLETON }),
+    server:        asClass(Server, { lifetime: Lifetime.SINGLETON }),
+    router:        asFunction(router, { lifetime: Lifetime.SINGLETON }),
+
+    // Middlewares
+    containerMiddleware: asValue(scopePerRequest(container)),
+
+    // Operations
+    addressWeatherHistory: asClass(AddressWeatherHistoryInteractor),
+
+    // Presenters
+    dailyWeatherPresenter: asClass(DailyWeatherPresenter),
+
+    // External APIs
+    weatherApi:  asClass(WeatherApi),
+    geocoderApi: asClass(GeocoderApi),
+    timezoneApi: asClass(TimezoneApi),
+
+    // Gateways
+    geocoderGateway: asClass(GeocoderGateway),
+    timezoneGateway: asClass(TimezoneGateway),
+    weatherGateway:  asClass(WeatherGateway),
+  });
+
+// Compose dependencies post cradle
+container.
+  register({
+    config: asValue(container.cradle.configGateway.compiled),
+  });
+
+module.exports = container;
